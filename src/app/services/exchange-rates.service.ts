cd5761f6668c9cde6eda4f6fe57585ab604b744f
nicolelele <nicolegerlach@hotmail.com>
2021-01-13 10:10:41 +0100

    1 file modified

--- src/app/services/exchange-rates.service.ts
+++ src/app/services/exchange-rates.service.ts
@@ -12,25 +12,30 @@ export interface ExchangeRatesResponse {
 
 @Injectable()
 export class ExchangeRatesService {
-  
-  private readonly BASE_URL = 'https://api.exchangeratesapi.io/latest?base=';
+  private readonly BASE_URL = "https://api.exchangeratesapi.io/latest?base=";
   private readonly ITEMS_PER_PAGE = 5;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getRates(currency: string, page = 1): Observable<MultiRates> {
-    return this.http.get<ExchangeRatesResponse>(`${this.BASE_URL}${currency}`)
-      .pipe(
-        map(response => {
-          return {
-            ...response,
-            rates: Object.entries(response.rates)
-            .map(([currency, value]) => ({ currency, value }))
-            .slice(this.ITEMS_PER_PAGE * (page - 1), this.ITEMS_PER_PAGE * page),
+    const url = `${this.BASE_URL}${currency}`;
 
-            pagesCount: Math.round(Object.keys(response.rates).length / this.ITEMS_PER_PAGE)
-          }
-        })
-      );
+    return this.http.get<ExchangeRatesResponse>(url).pipe(
+      map(response => {
+        return {
+          ...response,
+          rates: Object.entries(response.rates)
+            .map(([currency, value]) => ({ currency, value }))
+            .slice(
+              this.ITEMS_PER_PAGE * (page - 1),
+              this.ITEMS_PER_PAGE * page
+            ),
+
+          pagesCount: Math.round(
+            Object.keys(response.rates).length / this.ITEMS_PER_PAGE
+          )
+        };
+      })
+    );
   }
 }
