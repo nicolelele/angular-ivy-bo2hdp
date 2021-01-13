29c4d15c963455116c0e238ee348bda6abd51dce
nicolelele <nicolegerlach@hotmail.com>
2021-01-13 10:08:25 +0100

    1 file modified

--- src/app/components/exchange-rates/exchange-rates-table.component.ts
+++ src/app/components/exchange-rates/exchange-rates-table.component.ts
@@ -1,46 +1,51 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { ExchangeRatesService } from '../../services/exchange-rates.service';
-import { Observable, Subscription } from 'rxjs';
+import { Component, OnInit, Input, OnDestroy } from "@angular/core";
+import { ExchangeRatesService } from "../../services/exchange-rates.service";
+import { Observable, Subscription } from "rxjs";
 import { Rate, MultiRates } from "../../models/rates.model";
 
 @Component({
-  selector: 'exchange-rates-table',
-  templateUrl: './exchange-rates-table.component.html'
+  selector: "exchange-rates-table",
+  templateUrl: "./exchange-rates-table.component.html"
 })
 export class ExchangeRatesTableComponent implements OnInit, OnDestroy {
-  constructor(private exchangeRatesService: ExchangeRatesService) { }
+  constructor(private exchangeRatesService: ExchangeRatesService) {}
 
   private REFRESH_INTERVAL = 10000;
   currentPage = 1;
 
   @Input() currency: string;
   data: MultiRates;
-  subscribtion: Subscription;
-  refreshInterval: number;
+  subscription: Subscription;
+  refreshInterval;
 
-  ngOnInit(){
+  ngOnInit() {
     this.refresh();
-    this.refreshInterval = setTimeout(this.refresh, this.REFRESH_INTERVAL);
+    this.refreshInterval = setInterval(
+      () => this.refresh(),
+      this.REFRESH_INTERVAL
+    );
   }
 
   refresh() {
-    this.exchangeRatesService.getRates(this.currency, this.currentPage).subscribe(response => {
-      this.data = response;
-    });
-  };
+    this.subscription = this.exchangeRatesService
+      .getRates(this.currency, this.currentPage)
+      .subscribe(response => {
+        this.data = response;
+      });
+  }
 
   previousPage() {
     this.currentPage = this.currentPage - 1;
     this.refresh();
-  };
+  }
 
   nextPage() {
     this.currentPage = this.currentPage + 1;
     this.refresh();
-  };
+  }
 
   ngOnDestroy() {
-    this.subscribtion.unsubscribe();
+    this.subscription.unsubscribe();
     clearInterval(this.refreshInterval);
   }
 }
